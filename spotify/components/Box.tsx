602c4442d05e9b90e9d2ce3915638d121ba99a9a
Yusuf <yusufahmad5885@gmail.com>
2023-06-20 17:40:15 -0700

    frontend navbar, layout needs work

--- spotify/components/Box.tsx
+++ spotify/components/Box.tsx
@@ -1,25 +1,26 @@
-import {twMerge} from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
 interface BoxProps {
-    children: React.ReactNode;
-    className?:string,
+  children: React.ReactNode;
+  className?: string;
 }
 
-const Box: React.FC<BoxProps> = ({
-    children,
-    className
-}) => {
-    return (
-        <div
-            className={twMerge(`
+const Box: React.FC<BoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(
+        `
                 bg-neutral-900
                 rounded-lg
                 h-fit
                 w-full
-            `)}>
-            {children}
-        </div>
-    );
-}
+            `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Box;
