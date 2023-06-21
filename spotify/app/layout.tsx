497ba12fa3f68c276b8e0035ce6f19cf005d58cb
Yusuf <yusufahmad5885@gmail.com>
2023-06-21 15:42:57 -0700

    connected to db

--- spotify/app/layout.tsx
+++ spotify/app/layout.tsx
@@ -1,24 +1,27 @@
-import Sidebar from '@/components/Sidebar'
-import './globals.css'
-import { Figtree } from 'next/font/google'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to music!',
-}
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
-        </body>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
+      </body>
     </html>
-  )
+  );
 }
