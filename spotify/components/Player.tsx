8c90756c8aa671b851b148d60326d5e5c10bc7f3
Yusuf <yusufahmad5885@gmail.com>
2023-07-05 11:59:13 -0700

    pause and play

--- spotify/components/Player.tsx
+++ spotify/components/Player.tsx
@@ -3,14 +3,23 @@
 import useGetSongById from "@/hooks/UseGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
 
-  const songUrl = useLoadSongUrl(song);
+  const songUrl = useLoadSongUrl(song!);
 
-  return <div>Player!</div>;
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+    </div>
+  );
 };
 
 export default Player;
